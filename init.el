8ea36fa645b1b5818f1957a7881b44eb1551620b
qxxt <57898942+qxxt@users.noreply.github.com>
2023-06-15 13:27:38 +0700

    treemacs configuration

--- init.el
+++ init.el
@@ -132,6 +132,29 @@
   :config
   (load-theme 'doom-solarized-light t))
 
+(use-package treemacs
+  :ensure t
+  :demand t
+  :init
+  (setq treemacs-follow-after-init t
+        treemacs-indentation 1
+        treemacs-silent-refresh t
+        treemacs-silent-filewatch t
+        treemacs-is-never-other-window t
+        treemacs-sorting 'alphabetic-case-insensitive-asc)
+
+  :config
+  (treemacs-follow-mode t)
+  (treemacs-filewatch-mode t)
+
+  :bind
+  (:map global-map
+        ([f8] . treemacs)
+        ("C-c f" . treemacs-select-window))
+
+  :hook
+  ((prog-mode-hook vc-dir-mode-hook) . treemacs-display-current-project-exclusively))
+
 (use-package vertico
   :ensure t
   :demand t
