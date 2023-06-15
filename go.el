7e939cde2c661aba9bb9bc7228ddfefa31e4d241
qxxt <57898942+qxxt@users.noreply.github.com>
2023-06-15 19:55:27 +0700

    separate go config from init.el

---
+++ go.el
@@ -0,0 +1,45 @@
+(defun go-format-and-import ()
+  "Format and imports the required modules."
+  (interactive)
+  (unless (eq major-mode 'go-mode)
+    (message "err 1")
+    (error "Not in go-mode"))
+
+  (gofmt)
+  (if (bound-and-true-p eglot--managed-mode)
+      (ignore-errors (eglot-code-actions nil nil "source.organizeImports" t))
+    (goimports)))
+
+(defun go-eval-buffer ()
+  "Save current buffer as cache and run it with `go run`."
+  (interactive)
+  (go-format-and-import)
+  (let ((b (current-buffer))
+        (filepath (concat (getenv "HOME") "/.cache/gorun/" (format-time-string "%d-%m-%Y %H:%M:%S" (current-time)) ".go")))
+    (with-temp-buffer
+      (insert-buffer-substring b)
+      (when (re-search-forward (rx "package"
+                                   (one-or-more whitespace)
+                                   (group (one-or-more any))
+                                   word-boundary)
+                               nil t)
+        (replace-match "main" nil nil nil 1))
+      (write-region (point-min) (point-max) filepath))
+    (shell-command (concat "go run '" filepath "'"))))
+
+;; TODO
+(defun goimports ()
+  "Imports dependencies using goimport tool."
+  (interactive))
+
+(use-package go-mode
+  :init
+  (setq gofmt-command "gofumpt")
+
+  :bind (:map go-mode-map
+              ("C-c C-f" . go-format-and-import)
+              ("C-c C-e" . go-eval-buffer))
+
+  :hook
+  (go-mode-hook . (lambda()
+                            (add-hook 'before-save-hook 'go-format-and-import))))
--- init.el
+++ init.el
@@ -154,8 +154,7 @@
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
 
-  :bind
-  (:map global-map
+  :bind (:map global-map
         ([f8] . treemacs)
         ("C-c f" . treemacs-select-window))
 
@@ -192,6 +191,8 @@
   (org-mode-hook . org-bullets-mode))
 
 ;; Prog-mode
+(load (expand-file-name "go.el" user-emacs-directory))
+
 (use-package company
   :hook
   (eglot-managed-mode-hook . company-mode)
@@ -252,53 +253,6 @@
 
   :bind (:map nix-mode-map
               ("C-c C-f" . nix-format-buffer)))
-
-;; go-mode
-(defun go-format-and-import ()
-  "Format and imports the required modules."
-  (interactive)
-  (unless (eq major-mode 'go-mode)
-    (message "err 1")
-    (error "Not in go-mode"))
-
-  (gofmt)
-  (if (bound-and-true-p eglot--managed-mode)
-      (ignore-errors (eglot-code-actions nil nil "source.organizeImports" t))
-    (goimports)))
-
-(defun go-eval-buffer ()
-  "Save current buffer as cache and run it with `go run`."
-  (interactive)
-  (go-format-and-import)
-  (let ((b (current-buffer))
-        (filepath (concat (getenv "HOME") "/.cache/gorun/" (format-time-string "%d-%m-%Y %H:%M:%S" (current-time)) ".go")))
-    (with-temp-buffer
-      (insert-buffer-substring b)
-      (when (re-search-forward (rx "package"
-                                   (one-or-more whitespace)
-                                   (group (one-or-more any))
-                                   word-boundary)
-                               nil t)
-        (replace-match "main" nil nil nil 1))
-      (write-region (point-min) (point-max) filepath))
-    (shell-command (concat "go run '" filepath "'"))))
-
-;; TODO
-(defun goimports ()
-  "Imports dependencies using goimport tool."
-  (interactive))
-
-(use-package go-mode
-  :init
-  (setq gofmt-command "gofumpt")
-
-  :bind (:map go-mode-map
-              ("C-c C-f" . go-format-and-import)
-              ("C-c C-e" . go-eval-buffer))
-
-  :hook
-  (go-mode-hook . (lambda()
-                            (add-hook 'before-save-hook 'go-format-and-import))))
 
 ;; Reevaluate init file
 (defun reevaluate-init-file ()
