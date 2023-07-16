def177aa872899c9c937ac5e4327b831e422c947
qxxt <57898942+qxxt@users.noreply.github.com>
2023-07-16 09:15:33 -0400

    fix gofmt

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -9,32 +9,14 @@
   "Format and imports the required modules."
   (interactive)
   (unless (eq major-mode 'go-mode)
-    (message "err 1")
     (error "Not in go-mode"))
-
   (gofmt)
-  (if (bound-and-true-p eglot--managed-mode)
-      (ignore-errors (eglot-code-actions nil nil "source.organizeImports" t))
+
+  ;; https://github.com/golang/go/issues/60230
+  (unless (and (bound-and-true-p eglot--managed-mode)
+               (eglot-code-actions nil nil "source.organizeImports" t))
     (goimports)))
 
-(defun go-eval-buffer ()
-  "Save current buffer as cache and run it with `go run`."
-  (interactive)
-  (go-format-and-import)
-  (let ((b (current-buffer))
-    (filepath (concat (getenv "HOME") "/.cache/gorun/" (format-time-string "%d-%m-%Y %H:%M:%S" (current-time)) ".go")))
-    (with-temp-buffer
-      (insert-buffer-substring b)
-      (when (re-search-forward (rx "package"
-                   (one-or-more whitespace)
-                   (group (one-or-more any))
-                   word-boundary)
-                   nil t)
-    (replace-match "main" nil nil nil 1))
-      (write-region (point-min) (point-max) filepath))
-    (shell-command (concat "go run '" filepath "'"))))
-
-;; TODO
 (defun goimports ()
   "Imports dependencies using goimport tool."
   (interactive)
