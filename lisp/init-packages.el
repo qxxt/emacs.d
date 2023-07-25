62c6b77c5081a9b62e1c153ade7f90ed7051bbeb
qxxt <57898942+qxxt@users.noreply.github.com>
2023-07-25 13:17:14 -0400

    tweak auto-package-update and adding packages-list

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -3,68 +3,86 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'time-date)
+
 ;; Selected packages
 (setq package-selected-packages
       '(
-        ;; Interface
-        modus-themes dimmer delight
+	;; Interface
+	modus-themes dimmer delight
 
-        ;; Package
-        use-package auto-package-update
+	;; Package
+	use-package auto-package-update
 
-        ;; Texts utils
-        consult rainbow-mode rainbow-delimiters undo-fu iedit yasnippet
-        ;; go-snippets py-snippets common-lisp-snippets
-        ;; haskell-snippets
+	;; Texts utils
+	consult rainbow-mode rainbow-delimiters undo-fu iedit
+	yasnippet
 
-        ;; Minibuffer
-        vertico marginalia
+	;; Minibuffer
+	vertico marginalia
 
-        ;; Git
-        magit diff-hl
+	;; Git
+	magit
 
-        ;; Treemacs
-        treemacs treemacs-magit
+	;; Treemacs
+	treemacs treemacs-magit
 
-        ;; Prog-mode
-        corfu eglot shfmt python-mode haskell-mode dockerfile-mode
+	;; Prog-mode
+	corfu eglot shfmt python-mode haskell-mode
+	dockerfile-mode diff-hl
 
-        ;; Golang
-        go-mode
+	;; Golang
+	go-mode
 
-        ;; Rust
-        rust-mode
+	;; Rust
+	rust-mode
 
-        ;; *Lisp
-        slime
+	;; *Lisp
+	slime lispy
 
-        ;; Org-mode
-        org-bullets ob-go ob-rust
+	;; Org-mode
+	org-bullets ob-go ob-rust
 
-        ;; Misc
-        which-key
-        ))
+	;; Help
+	which-key))
 
-;; `time-to-number-of-days' depends upon `time-date'
-(require 'time-date)
 (use-package auto-package-update
   :ensure t
   :demand t
   :init
-  (setq auto-package-update-interval 1
-        auto-package-update-delete-old-versions t
-        auto-package-update-prompt-before-update t
-        auto-package-update-show-preview t)
+  (setq auto-package-update-interval 3
+	auto-package-update-delete-old-versions t
+	auto-package-update-prompt-before-update t
+	auto-package-update-show-preview t)
 
   :config
-  ;; Update `package-archives' and upgrade packages when it's last
-  ;; updated `auto-package-update-interval' days ago, with prompts.
-  (if (and (<= auto-package-update-interval
-               (time-to-number-of-days
-                (time-since
-                 (file-attribute-modification-time (file-attributes (concat package-user-dir "/archives/gnu/archive-contents"))))))
-           (y-or-n-p "Update packages now? "))
-      (auto-package-update-maybe)))
+  (defun apu-package-refresh-content? ()
+    (when (and
+	   (or (not package-archive-contents)
+	       (not (> auto-package-update-interval
+		       (time-to-number-of-days
+			(time-since
+			 (file-attribute-modification-time
+			  (file-attributes
+			   (concat package-user-dir "/archives/gnu/archive-contents"))))))))
+	   (y-or-n-p "Refresh packages now?"))
+      (package-refresh-contents)
+      t))
+
+  (defun apu-update-list? ()
+    (let* ((package-to-update-list (apu--filter-quelpa-packages (apu--packages-to-install)))
+	   update-then?)
+      (if (null package-to-update-list)
+	  (message "All packages up to date")
+	(apu--write-preview-buffer (concat "[PACKAGES TO UPDATE]:\n" (mapconcat #'symbol-name package-to-update-list "\n")))
+	(setq update-then? (y-or-n-p "Update packages now?"))
+	(apu--hide-preview)
+	update-then?)))
+
+  (if (and (not (> auto-package-update-interval (- (apu--today-day) (apu--read-last-update-day))))
+	     (apu-package-refresh-contents?)
+	     (apu-update-list?))
+      (auto-package-update-now)))
 
 (provide 'init-packages)
 ;;; init-packages.el ends here
